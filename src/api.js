83b1fb600f34fdadb719c55cc33f20d74eda4781
saliegh_aziz <saliegh_aziz@users.noreply.github.com>
2017-08-09 16:09:59 +0200

    API timeout changed

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
                         baseURL: 'http://frontendshowcase.azurewebsites.net/api/',
-                        timeout: 1000,
+                        timeout: 5000,
                         headers: {'X-Custom-Header': 'foobar'}
                         });
 
