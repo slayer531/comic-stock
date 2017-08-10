6a4a8b201fded84affe88d9599e15d7846e9be84
saliegh_aziz <saliegh_aziz@users.noreply.github.com>
2017-08-10 14:41:48 +0200

    Supplierlist changes
    applied alt row colour. key changed index

--- src/SupplierList.js
+++ src/SupplierList.js
@@ -10,16 +10,16 @@ const SupplierList = props =>
             <div className="col-md-3">Reference</div>
             <div className="col-md-1">Delete</div>
         </div>
-        <div className="row">
+        <div className=".row">
             {props.suppliers.map((supplier,index)  =>(
-                        <div key={supplier.id} index={supplier.id}>
-                            <div className="col-md-1"><button onClick={() => props.Edit(supplier.id)}>Edit</button></div>
+                        <div key={index} className="row erdiv">
+                            <div className="col-md-1"><button onClick={() => props.Edit(supplier)}>Edit</button></div>
                             <div className="col-md-4">{supplier.name}</div>
                             <div className="col-md-3">{supplier.city}</div>
                             <div className="col-md-3">{supplier.reference}</div>  
                             <div className="col-md-1"><img src={DeleteImage} alt="delete me" onClick={() => props.Delete(supplier.id)} /></div>
                         </div>                      
-                    ))}
+            ))}
         </div>
                                          
     </div>
