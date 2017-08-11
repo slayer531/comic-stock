bdecbda4d0ae19c79d8969147253f37f511eee8c
saliegh_aziz <saliegh_aziz@users.noreply.github.com>
2017-08-11 11:42:27 +0200

    formatting

--- src/SupplierList.jsx
+++ src/SupplierList.jsx
@@ -1,43 +1,43 @@
-import React from 'react';
-import DeleteImage from './images/delete-x-square-button.png';
+import React from "react";
+import DeleteImage from "./images/delete-x-square-button.png";
 
 function SupplierList(props) {
-    return(
-        <div>
-    <div className="row">
-      <div className="col-md-1">Edit</div>
-      <div className="col-md-4">Name</div>
-      <div className="col-md-3">City</div>
-      <div className="col-md-3">Reference</div>
-      <div className="col-md-1">Delete</div>
+  return (
+    <div>
+      <div className="row">
+        <div className="col-md-1">Edit</div>
+        <div className="col-md-4">Name</div>
+        <div className="col-md-3">City</div>
+        <div className="col-md-3">Reference</div>
+        <div className="col-md-1">Delete</div>
+      </div>
+      <div className=".row">
+        {props.suppliers.map((supplier, index) =>
+          <div key={index} className="row erdiv">
+            <div className="col-md-1">
+              <button onClick={() => props.Edit(supplier)}>Edit</button>
+            </div>
+            <div className="col-md-4">
+              {supplier.name}
+            </div>
+            <div className="col-md-3">
+              {supplier.city}
+            </div>
+            <div className="col-md-3">
+              {supplier.reference}
+            </div>
+            <div className="col-md-1">
+              <img
+                src={DeleteImage}
+                alt="delete me"
+                onClick={() => props.Delete(supplier.id)}
+              />
+            </div>
+          </div>
+        )}
+      </div>
     </div>
-    <div className=".row">
-      {props.suppliers.map((supplier, index) =>
-        <div key={index} className="row erdiv">
-          <div className="col-md-1">
-            <button onClick={() => props.Edit(supplier)}>Edit</button>
-          </div>
-          <div className="col-md-4">
-            {supplier.name}
-          </div>
-          <div className="col-md-3">
-            {supplier.city}
-          </div>
-          <div className="col-md-3">
-            {supplier.reference}
-          </div>
-          <div className="col-md-1">
-            <img
-              src={DeleteImage}
-              alt="delete me"
-              onClick={() => props.Delete(supplier.id)}
-            />
-          </div>
-        </div>
-      )}
-    </div>
-  </div>
-    );
+  );
 }
 
 export default SupplierList;
