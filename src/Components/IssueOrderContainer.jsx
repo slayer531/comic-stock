1eb6212ae3b5aa56572cc4f46bf79cbe5d5478ce
saliegh_aziz <saliegh_aziz@users.noreply.github.com>
2017-08-16 18:54:10 +0200

    Refactored some Issue Order Code

--- src/Components/IssueOrderContainer.jsx
+++ src/Components/IssueOrderContainer.jsx
@@ -3,16 +3,34 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import IssueOrder from "../IssueOrder";
 import api from "./../api.js";
 
+const IssueCondition = {
+  VeryFine: 1,
+  Fine: 2,
+  Good: 3,
+  Poor: 4
+};
+
 class IssueOrderContainer extends React.Component {
   constructor(props) {
     super(props);
     this.initialiseState(props);
+    this.onAmountChangeHandle = this.onAmountChangeHandle.bind(this);
   }
 
   initialiseState(props) {
     this.state = {
       issue: props.Issue,
-      supplierData: []
+      supplierData: [],
+      issueOrderDetails: {
+        IssueId: 0,
+        SupplierId: 0,
+        IssueOrder: [
+          { Condition: IssueCondition.VeryFine, Amount: 0 },
+          { Condition: IssueCondition.Fine, Amount: 0 },
+          { Condition: IssueCondition.Good, Amount: 0 },
+          { Condition: IssueCondition.Poor, Amount: 0 }
+        ]
+      }
     };
   }
 
@@ -36,19 +54,28 @@ class IssueOrderContainer extends React.Component {
         console.error(e);
       });
   }
+
+  onAmountChangeHandle(event) {
+    console.log("onAmountChangeHandle:" + event.target.value);
+  }
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-md-4">
-            <IssueOrder suppliers={this.state.supplierData} />
-          </div>          
+            <IssueOrder
+              suppliers={this.state.supplierData}
+              issueOrderDetails={this.state.issueOrderDetails}
+              onAmountChange={this.onAmountChangeHandle}
+            />
+          </div>
         </div>
         <div className="row">
           <button onClick={() => this.CreateNewIssueOrder()}>
             {"Submit Order"}
           </button>
-           <button onClick={this.props.CancelNewOrder}>
+          <button onClick={this.props.CancelNewOrder}>
             {"Cancel"}
           </button>
         </div>
