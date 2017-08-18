5a1bd3705378be0ce15fb7957faf993b8ca077d6
saliegh_aziz <saliegh_aziz@users.noreply.github.com>
2017-08-18 17:34:06 +0200

    Added constant

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ import SupplierEditorContainer from "./Components/SupplierEditorContainer.jsx";
 import IssueEditorContainer from "./Components/IssueEditorContainer.jsx";
 import NavigationBar from "./NavigationBar.jsx";
 import { BrowserRouter, Route } from "react-router-dom";
-import Constants from "./Constants.jsx";
+ import Constants from "./Constants.jsx"; 
+import {
+    APP_HOME, APP_ISSUES_URL, APP_ORDERS_URL, APP_SUPPLIERS_ADD_URL,
+    APP_SUPPLIERS_EDIT_URL, APP_SUPPLIERS_URL
+} from "./Constants";
 
 class ComicStrip extends React.Component {
   constructor() {
@@ -22,16 +26,18 @@ class ComicStrip extends React.Component {
     return (
       <div className="container">
         <NavigationBar />
-        <Route path= {Constants.APP_SUPPLIERS_URL} component={SupplierEditorContainer} />
-        <Route path={Constants.APP_ISSUES_URL} component={IssueEditorContainer} />
+        {/* <Route path={"/suppliers/"} component={SupplierEditorContainer} />
+        <Route path={"/issues/"} component={IssueEditorContainer} /> */}
+         <Route path={APP_SUPPLIERS_URL} component={SupplierEditorContainer} />
+        <Route path={APP_ISSUES_URL} component={IssueEditorContainer} /> 
       </div>
     );
   }
 }
 
   ReactDOM.render(
-  <BrowserRouter basename="/comic-strip">
-     <Route path={Constants.APP_HOME} component={ComicStrip}/>
+  <BrowserRouter>
+     <Route path={APP_HOME} component={ComicStrip}/>
   </BrowserRouter>,
   document.getElementById("root")
 );  
