77761953f95ac234bb528b5fe741ee0211999ef5
saliegh_aziz <saliegh_aziz@users.noreply.github.com>
2017-08-18 10:05:14 +0200

    replaced function expressions with declarations

--- src/SupplierEditor.jsx
+++ src/SupplierEditor.jsx
@@ -10,61 +10,64 @@ const PageState = {
   Saved: 4
 };
 
-const AddNewVisibility = (props) => {
-    if (props.PageState === PageState.List) {
-      return (
-        <div className="col-md-1 col-md-offset-11">
-          <img
-            src={AddSupplierImage}
-            alt="Add new supplier"
-            onClick={i => props.AddNew(i)}
-          />
-        </div>
-      );
-    }
-  }
-
-const ControlsToShow = (props) => {
-    switch (props.PageState) {
-      case PageState.List:
-        return <SupplierListContainer EditSupplier={i => props.EditSupplier(i)} />;
-      case PageState.New:
-        return (
-          <SupplierDetailContainer
-            id={0}
-            name={""}
-            city={""}
-            reference={""}
-            SaveSupplier={i => props.SaveSupplier(i)}
-            setPageMode={props.setPageMode}
-          />
-        );
-      case PageState.Edit:
-        return (
-          <SupplierDetailContainer
-            id={props.id}
-            name={props.name}
-            city={props.city}
-            reference={props.reference}
-            SaveSupplier={i => props.SaveSupplier(i)}
-            setPageMode={props.setPageMode}
-          />
-        );
-      case PageState.Saved:
-        return <SupplierListContainer EditSupplier={i => props.EditSupplier(i)} />;
-      default:
-        alert("Invalid Page State encountered");
-    }
-  }
-
-function SupplierEditor(props) {
-
-  return (
-      <div className="container">        
-        {AddNewVisibility(props)}
-        {ControlsToShow(props)}
+function AddNewVisibility(props) {
+  if (props.PageState === PageState.List) {
+    return (
+      <div className="col-md-1 col-md-offset-11">
+        <img
+          src={AddSupplierImage}
+          alt="Add new supplier"
+          onClick={i => props.AddNew(i)}
+        />
       </div>
     );
+  }
+}
+
+function ControlsToShow(props) {
+  switch (props.PageState) {
+    case PageState.List:
+      return (
+        <SupplierListContainer EditSupplier={i => props.EditSupplier(i)} />
+      );
+    case PageState.New:
+      return (
+        <SupplierDetailContainer
+          id={0}
+          name={""}
+          city={""}
+          reference={""}
+          SaveSupplier={i => props.SaveSupplier(i)}
+          setPageMode={props.setPageMode}
+        />
+      );
+    case PageState.Edit:
+      return (
+        <SupplierDetailContainer
+          id={props.id}
+          name={props.name}
+          city={props.city}
+          reference={props.reference}
+          SaveSupplier={i => props.SaveSupplier(i)}
+          setPageMode={props.setPageMode}
+        />
+      );
+    case PageState.Saved:
+      return (
+        <SupplierListContainer EditSupplier={i => props.EditSupplier(i)} />
+      );
+    default:
+      alert("Invalid Page State encountered");
+  }
+};
+
+function SupplierEditor(props) {
+  return (
+    <div className="container">
+      {AddNewVisibility(props)}
+      {ControlsToShow(props)}
+    </div>
+  );
 }
 
 export default SupplierEditor;
