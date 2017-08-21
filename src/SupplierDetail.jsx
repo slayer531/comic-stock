de792f6ec2dbceabbe84e0f5dbf3e8b478a97e3b
saliegh_aziz <saliegh_aziz@users.noreply.github.com>
2017-08-21 09:57:23 +0200

    rearranged buttons

--- src/SupplierDetail.jsx
+++ src/SupplierDetail.jsx
@@ -10,23 +10,23 @@ function SupplierDetail(props) {
       <div className="row">
         <div className="col-md-2 col-md-offset-2">Name</div>
         <input value={props.name} onChange={props.setName} />
-        <input className="hiddenControl" value={props.id} />  
+        <input className="hiddenControl" value={props.id} />
       </div>
-       <div className="row">
+      <div className="row">
         <div className="col-md-2 col-md-offset-2">City</div>
         <input value={props.city} onChange={props.setCity} />
       </div>
-       <div className="row">        
+      <div className="row">
         <div className="col-md-2 col-md-offset-2">Reference</div>
         <input value={props.reference} onChange={props.setReference} />
       </div>
-      <br/>
+      <br />
       <div className="row">
         <div className="col-md-2 col-md-offset-3">
-          <Button onClick={i => props.SaveSupplier(i)}>SAVE</Button>
+          <Button onClick={i => props.Cancel(i)}>CANCEL</Button>
         </div>
-        <div className="col-md-2 col-md-offset-1">    
-          <Button onClick={i => props.Cancel(i)}>CANCEL</Button>    
+        <div className="col-md-2 col-md-offset-1">
+          <Button onClick={i => props.SaveSupplier(i)}>SAVE</Button>
         </div>
       </div>
     </div>
