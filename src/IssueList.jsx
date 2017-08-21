eb815ce26afcabfd16351776af4d6f9355d8f70a
saliegh_aziz <saliegh_aziz@users.noreply.github.com>
2017-08-21 18:02:58 +0200

    tidy up issue list

--- src/IssueList.jsx
+++ src/IssueList.jsx
@@ -1,24 +1,28 @@
 import React from "react";
 import NoImage from "./images/not_available_icon.jpg";
-import Thumbnail from "react-bootstrap/lib/Thumbnail";
+import Image from "react-bootstrap/lib/Image";
+import  Col from "react-bootstrap/lib/Col";
 
 function IssueList(props) {
   let issues = props.Issues;
 
   return (
     <div>
-        {issues.map((issue, index) =>
-            <Thumbnail key={index}
-                    src={
-                      issue.images.length > 0
-                        ? issue.images[0].pathIncludingExtension
-                        : NoImage
-                    }
-                    onClick={() => props.View(issue.id)}
-                    alt="171x180"
-                  />
-          )}
-      </div>
+      {issues.map((issue, index) =>
+        <div className="thumbnail">
+          <Image
+            key={index}
+            src={
+              issue.images.length > 0
+                ? issue.images[0].pathIncludingExtension
+                : NoImage
+            }
+            onClick={() => props.View(issue.id)}
+            rounded
+          />
+        </div>
+      )}
+    </div>
   );
 }
 
