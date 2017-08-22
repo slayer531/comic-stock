49641cdec4a43d0fc894f598f36645483a729d26
saliegh_aziz <saliegh_aziz@users.noreply.github.com>
2017-08-22 08:26:58 +0200

    split issues into 2x3 item arrays

--- src/IssueEditor.jsx
+++ src/IssueEditor.jsx
@@ -5,7 +5,7 @@ import IssueListContainer from "./Components/IssueListContainer";
 function IssueEditor(props) {
   return (
      <div className="row">
-        <div className="col-md-6">
+        <div className="col-md-4">
           <IssueListContainer View={i => props.View(i)} />
         </div>
         <div className="col-md-6">
--- src/IssueList.jsx
+++ src/IssueList.jsx
@@ -1,27 +1,45 @@
 import React from "react";
 import NoImage from "./images/not_available_icon.jpg";
 import Image from "react-bootstrap/lib/Image";
-import  Col from "react-bootstrap/lib/Col";
 
 function IssueList(props) {
-  let issues = props.Issues;
+  let issuesFirstHalf = props.Issues.slice(0, 3);
+  let issuesSecondHalf = props.Issues.slice(3);
 
   return (
-    <div>
-      {issues.map((issue, index) =>
-        <div className="thumbnail">
-          <Image
-            key={index}
-            src={
-              issue.images.length > 0
-                ? issue.images[0].pathIncludingExtension
-                : NoImage
-            }
-            onClick={() => props.View(issue.id)}
-            rounded
-          />
-        </div>
-      )}
+    <div className="row">
+      <div className="col-md-6">
+        {issuesFirstHalf.map((issue, index) =>
+          <div className="col-md-12 thumbnail">
+            <Image
+              key={index}
+              src={
+                issue.images.length > 0
+                  ? issue.images[0].pathIncludingExtension
+                  : NoImage
+              }
+              onClick={() => props.View(issue.id)}
+              rounded
+            />
+          </div>
+        )}
+      </div>
+      <div className="col-md-6">
+        {issuesSecondHalf.map((issue, index) =>
+          <div className="col-md-12 thumbnail">
+            <Image
+              key={index}
+              src={
+                issue.images.length > 0
+                  ? issue.images[0].pathIncludingExtension
+                  : NoImage
+              }
+              onClick={() => props.View(issue.id)}
+              rounded
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
